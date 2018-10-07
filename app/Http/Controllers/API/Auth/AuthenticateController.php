813c46f1a54f8ade12a79cc7811719216ecf9d74
José Cage <josejanuario7@gmail.com>
2018-10-08 00:40:45 +0100

    :muscle: Adicionando rotas de autenticação e das gírias

---
+++ app/Http/Controllers/API/Auth/AuthenticateController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kallun\Http\Controllers\API\Auth;
+
+use Illuminate\Http\Request;
+use Kallun\Http\Controllers\Controller;
+
+class AuthenticateController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/API/SlangsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kallun\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Kallun\Http\Controllers\Controller;
+
+class SlangsController extends Controller
+{
+    //
+}
